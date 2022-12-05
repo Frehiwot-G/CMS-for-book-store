3cad43e2153b8fa8316df481cd1d8907dd8d819a
Frehiwot-G <frehiwotgem@gmail.com>
2022-12-04 21:06:15 -0800

    change commited

--- library/settings.py
+++ library/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIR =[
 ]
 MEDIA_ROOT= os.path.join(BASE_DIR,'static/images')
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST=config('EMAIL_HOST')
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER=config('EMAIL_HOST_USER')
